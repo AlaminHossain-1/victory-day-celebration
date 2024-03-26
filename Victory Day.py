bde7650463037a1eabf2b27ca4afb226af112ecc
AlaminHossain-1 <155953783+AlaminHossain-1@users.noreply.github.com>
2024-03-26 16:08:40 +0600

    Add files via upload

--- Victory Day.py
+++ Victory Day.py
@@ -2,10 +2,14 @@ from datetime import date
 
 def celebrate_BD_victory_day ():
     print("Happy Victory Day of Bangladesh!")
+def birthday_celebrate_BD ():
+    print("Happy Birthday Day Bangladesh!")
 
 today = date.today()
 d2 = today.strftime("%B %d, %Y")
 print("Today is", d2)
 
-if today.strftime("%B %d")=="December 16":
+if today.strftime("%B %d") == "December 16":
     celebrate_BD_victory_day()
+elif today.strftime("%B %d") == "March 26":
+    birthday_celebrate_BD()
